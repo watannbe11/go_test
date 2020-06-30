5380fcf7859c645990eed8e25fbdd2372d2e5199
yuichiro watanabe <yuuichiro1139@gmail.com>
2020-07-01 00:12:27 +0900

    crud

--- main.go
+++ main.go
@@ -1,23 +1,86 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-     "github.com/jinzhu/gorm"
+	"strconv"
 
-      _ "github.com/mattn/go-sqlite3"
+  "github.com/gin-gonic/gin"
+  "github.com/jinzhu/gorm"
+
+  _ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
     router := gin.Default()
     router.LoadHTMLGlob("templates/*.html")
-    data := "Hello Go/Gin!!"
 
-    router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{"data": data})
+    dbInit()
+
+    // index
+    router.GET("/", func(ctx *gin.Context) {
+        todos := dbGetAll()
+        ctx.HTML(200, "index.html", gin.H{
+            "todos": todos,
+        })
+    })
+
+    //create
+    router.POST("/new", func(ctx *gin.Context) {
+        text := ctx.PostForm("text")
+        status := ctx.PostForm("status")
+        dbInsert(text, status)
+        ctx.Redirect(302, "/")
+    })
+
+    //detail
+    router.GET("/detail/:id", func(ctx *gin.Context) {
+        n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic(err)
+        }
+        todo := dbGetOne(id)
+        ctx.HTML(200, "detail.html", gin.H{"todo": todo})
+    })
+
+    //update
+    router.POST("/update/:id", func(ctx *gin.Context) {
+        n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic("ERROR")
+        }
+        text := ctx.PostForm("text")
+        status := ctx.PostForm("status")
+        dbUpdate(id, text, status)
+        ctx.Redirect(302, "/")
+    })
+
+    //削除確認
+    router.GET("/delete_check/:id", func(ctx *gin.Context) {
+        n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic("ERROR")
+        }
+        todo := dbGetOne(id)
+        ctx.HTML(200, "delete.html", gin.H{"todo": todo})
+    })
+
+    //Delete
+    router.POST("/delete/:id", func(ctx *gin.Context) {
+        n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic("ERROR")
+        }
+        dbDelete(id)
+        ctx.Redirect(302, "/")
+
     })
 
     router.Run()
 }
+
 
 // db
 type Todo struct {
